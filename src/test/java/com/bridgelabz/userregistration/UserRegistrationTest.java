03aaeab2313e6d87b5aa1c43b4821b2e3f556ed2
Textspot <sachinkatarki1995@gmail.com>
2022-01-30 14:46:10 +0530

    Added:Programme to User need to Enter a valid Email

--- src/test/java/com/bridgelabz/userregistration/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/userregistration/UserRegistrationTest.java
@@ -30,4 +30,16 @@ public class UserRegistrationTest {
 		boolean result = userRegistration.lastName("kumar");
 		Assert.assertEquals(false, result);
 	}
+
+	@Test
+	public void givenEmail_WhenProper_ShouldReturnTrue() {
+		boolean result = userRegistration.email("sachin2022@gmail.com");
+		Assert.assertEquals(true, result);
+	}
+
+	@Test
+	public void givenEmail_WhenNotProper_ShouldReturnFalse() {
+		boolean result = userRegistration.email("sachin&kumar@gmail.com");
+		Assert.assertEquals(false, result);
+	}
 }
